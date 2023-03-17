d07321d8361a3129e53f7a6aa74c88eed0b22cf1
dobromirts <46093564+dobromirts@users.noreply.github.com>
2023-03-17 16:30:44 +0200

    untitled-app updates

--- src/app/view1/view1.ts
+++ src/app/view1/view1.ts
@@ -1,8 +1,8 @@
 import { html, css, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import { defineComponents, IgcCalendarComponent } from 'igniteui-webcomponents';
+import { defineComponents, IgcButtonComponent, IgcCalendarComponent, IgcCardComponent, IgcIconButtonComponent, IgcIconComponent, IgcRippleComponent } from 'igniteui-webcomponents';
 
-defineComponents(IgcCalendarComponent);
+defineComponents(IgcCalendarComponent, IgcCardComponent, IgcButtonComponent, IgcRippleComponent, IgcIconButtonComponent, IgcIconComponent);
 
 @customElement('app-view1')
 export default class View1 extends LitElement {
@@ -29,6 +29,11 @@ export default class View1 extends LitElement {
       flex-grow: 1;
       flex-basis: 0;
     }
+    .card {
+      width: 320px;
+      height: max-content;
+      min-width: 320px;
+    }
     .h5 {
       margin: 0 0 16px;
       height: max-content;
@@ -41,6 +46,18 @@ export default class View1 extends LitElement {
     .calendar {
       width: max-content;
       height: max-content;
+    }
+    .body-content {
+      min-width: 50px;
+      min-height: 50px;
+    }
+    .actions-content {
+      min-width: 50px;
+      min-height: 40px;
+    }
+    .button {
+      height: max-content;
+      min-width: min-content;
     }
   `;
 
@@ -57,6 +74,41 @@ export default class View1 extends LitElement {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
         <igc-calendar ?hide-header="${false}" header-orientation="horizontal" class="calendar"></igc-calendar>
+        <igc-card class="card">
+          <igc-card-header>
+            <h3 slot="title">
+              Title goes here...
+            </h3>
+            <h5 slot="subtitle">
+              Subtitle goes here...
+            </h5>
+          </igc-card-header>
+          <igc-card-content class="body-content"></igc-card-content>
+          <igc-card-actions class="actions-content">
+            <igc-button variant="flat" class="button">
+              Button
+              <igc-ripple></igc-ripple>
+            </igc-button>
+            <igc-icon-button variant="flat">
+              <span class="material-icons">
+                favorite
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+            <igc-icon-button variant="flat">
+              <span class="material-icons">
+                bookmark
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+            <igc-icon-button variant="flat">
+              <span class="material-icons">
+                share
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+          </igc-card-actions>
+        </igc-card>
       </div>
     `;
   }
